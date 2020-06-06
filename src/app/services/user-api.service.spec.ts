ecc3e141f83187fc2b857185ead43f4ffd1ebc86
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-06-06 12:08:15 +0200

    test user API GET

--- src/app/services/user-api.service.spec.ts
+++ src/app/services/user-api.service.spec.ts
@@ -1,18 +1,49 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UserApiService } from './user-api.service';
+import { Person } from '../models/person';
+import { personMockResponse } from '../mocks/person-api';
 
-describe('UserApiService', () => {
+fdescribe('UserApiService', () => {
+  const urlApi = 'https://jsonplaceholder.typicode.com/users/';
   let service: UserApiService;
+  let httpTestingController: HttpTestingController;
+  let request;
+  const launchSyncRequest = (URL: string = '', mockResponse: Object = {}, method: string = 'GET') => {
+    request = httpTestingController.expectOne(URL);
+    expect(request.request.method).toBe(method);
+    request.flush(mockResponse);
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserApiService]
+      providers: [UserApiService],
+      imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(UserApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('test get User data', () => {
+    const personId = 2;
+    const personGet = `${urlApi}/${personId}`
+    it('should return user by id', () => {
+      service.getUserById(personId).subscribe((person: Person) => {
+        expect(person.id).toBeDefined();
+        expect(person.username).toBeDefined();
+        expect(person.email).toBeDefined();
+        expect(person.phone).toBeDefined();
+      });
+      launchSyncRequest(personGet, personMockResponse);
+    });
   });
 });
--- src/app/services/user-api.service.ts
+++ src/app/services/user-api.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Person } from '../models/person';
 
 @Injectable()
 export class UserApiService {
+  private static readonly URL = 'https://jsonplaceholder.typicode.com/users/';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  private static getUser(id: number): string {
+    return `${UserApiService.URL}/${id}`;
+  }
+
+  getUserById(id: number): Observable<Person> {
+    return this.httpClient.get<Person>(UserApiService.getUser(id));
+  }
 }
