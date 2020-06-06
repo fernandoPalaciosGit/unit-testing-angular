3611ed4088342d8917ff62d481852b018ccda39b
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-06-06 13:34:31 +0200

    test error http response

--- src/app/services/user-api.service.spec.ts
+++ src/app/services/user-api.service.spec.ts
@@ -1,20 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 
 import { UserApiService } from './user-api.service';
 import { Person } from '../models/person';
 import { personMockResponse } from '../mocks/person-api';
+import { HttpErrorResponse } from '@angular/common/http';
 
-fdescribe('UserApiService', () => {
+describe('UserApiService', () => {
   const urlApi = 'https://jsonplaceholder.typicode.com/users/';
   let service: UserApiService;
   let httpTestingController: HttpTestingController;
-  let request;
-  const launchSyncRequest = (URL: string = '', mockResponse: Object = {}, method: string = 'GET') => {
-    request = httpTestingController.expectOne(URL);
-    expect(request.request.method).toBe(method);
-    request.flush(mockResponse);
-  };
+  let request: TestRequest;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -35,15 +31,36 @@ fdescribe('UserApiService', () => {
 
   describe('test get User data', () => {
     const personId = 2;
-    const personGet = `${urlApi}/${personId}`
+    const personGet = `${urlApi}/${personId}`;
+
     it('should return user by id', () => {
-      service.getUserById(personId).subscribe((person: Person) => {
+      const callbackSuccess = (person: Person) => {
         expect(person.id).toBeDefined();
         expect(person.username).toBeDefined();
         expect(person.email).toBeDefined();
         expect(person.phone).toBeDefined();
-      });
-      launchSyncRequest(personGet, personMockResponse);
+      };
+      const httpObserver = { next: callbackSuccess };
+      spyOn(httpObserver, 'next');
+      service.getUserById(personId).subscribe(httpObserver);
+      request = httpTestingController.expectOne(personGet);
+      request.flush(personMockResponse);
+      expect(httpObserver.next).toHaveBeenCalled();
+    });
+
+    it('should throw error request', () => {
+      const errorMessage = 'Invalid request parameters';
+      const errorResponse = { status: 400, statusText: 'Bad Request' };
+      const callbackError = (data: HttpErrorResponse) => {
+        expect(data.error).toBe(errorMessage);
+        expect(data.status).toBe(errorResponse.status);
+      };
+      const httpObserver = { error: callbackError };
+      spyOn(httpObserver, 'error');
+      service.getUserById(personId).subscribe(httpObserver);
+      request = httpTestingController.expectOne(personGet);
+      request.flush(errorMessage, errorResponse);
+      expect(httpObserver.error).toHaveBeenCalled();
     });
   });
 });
